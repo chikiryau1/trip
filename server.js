3358659e4c2f854a2bc9d737ae20b25b1e833548
ilyaqwerty <topmastertop97@gmail.com>
2019-07-08 06:50:05 +0300

    idk

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express'
-import cors from 'cors'
-import path from 'path'
-import favicon from 'express-favicon'
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const favicon = require('express-favicon')
 
 const PORT = process.env.PORT || 8000;
 
